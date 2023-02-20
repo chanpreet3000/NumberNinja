12f54abb172487fb86e515672adbc550edb5ef87
Chanpreet Singh <chanpreet3000@gmail.com>
2023-02-20 21:10:16 +0530

    Basic KeyPad and Functionality added

--- app/src/main/java/com/chanpreet/numberninja/GameInstance.java
+++ app/src/main/java/com/chanpreet/numberninja/GameInstance.java
@@ -1,108 +1,75 @@
 package com.chanpreet.numberninja;
 
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import com.chanpreet.numberninja.databinding.ItemMatrixCellBinding;
 
 import java.util.List;
 
 public class GameInstance {
-    private int row;
-    private int column;
+    private int currentRow;
 
-    private final Context context;
     private final MatrixBuilder.MatrixInfo matrixInfo;
+    private String currentEquation = "";
+    private final List<List<ItemMatrixCellBinding>> listOfBindings;
+    private final Context context;
 
     public GameInstance(Context context, MatrixBuilder.MatrixInfo matrixInfo) {
         this.context = context;
         this.matrixInfo = matrixInfo;
-        row = -1;
-        column = 0;
+        currentRow = -1;
+        listOfBindings = matrixInfo.getListOfBindings();
+        moveToNextRow();
+    }
+
+    public void pushCharacter(char c) {
+        if (currentEquation.length() < matrixInfo.getColumns()) {
+            currentEquation += c;
+        }
+        changeEquationVisuals();
+    }
 
 
-        List<List<ItemMatrixCellBinding>> listOfBindings = matrixInfo.getListOfBindings();
+    public void removeCharacter() {
+        if (currentEquation.length() > 0) {
+            currentEquation = currentEquation.substring(0, currentEquation.length() - 1);
+        }
+        changeEquationVisuals();
+    }
+
+    private void changeEquationVisuals() {
+        for (int i = 0; i < currentEquation.length(); i++) {
+            listOfBindings.get(currentRow).get(i).textView.setText(String.valueOf(currentEquation.charAt(i)));
+        }
+        for (int i = currentEquation.length(); i < matrixInfo.getColumns(); i++) {
+            listOfBindings.get(currentRow).get(i).textView.setText(null);
+        }
+    }
+
+    public void CheckButtonClicked() {
+        if (preCheckCondition()) {
+            moveToNextRow();
+        }
+    }
+
+    private boolean preCheckCondition() {
+        return (currentEquation.length() == matrixInfo.getColumns());
+    }
+
+
+    private void moveToNextRow() {
+        if (currentRow == matrixInfo.getRows() - 1) {
+            Toast.makeText(context, "Game has Ended. No tries left", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        currentRow++;
+        currentEquation = "";
+
         for (int i = 0; i < matrixInfo.getRows(); i++) {
             for (int j = 0; j < matrixInfo.getColumns(); j++) {
-                EditText curr = listOfBindings.get(i).get(j).editText, next = null, prev = null;
-                if (j < matrixInfo.getColumns() - 1) {
-                    next = listOfBindings.get(i).get(j + 1).editText;
-                }
-                if (j > 0) {
-                    prev = listOfBindings.get(i).get(j - 1).editText;
-                }
-                listOfBindings.get(i).get(j).editText.addTextChangedListener(new GameInstance.GenericTextWatcher(curr, next));
-                listOfBindings.get(i).get(j).editText.setOnKeyListener(new GameInstance.GenericKeyEvent(curr, prev));
-
-            }
-        }
-
-    }
-
-
-    public void moveToNextRow() {
-        List<List<ItemMatrixCellBinding>> listOfBindings = matrixInfo.getListOfBindings();
-        if (row != -1) {
-            for (int i = 0; i < matrixInfo.getColumns(); i++) {
-                listOfBindings.get(row).get(i).editText.setEnabled(false);
-            }
-        }
-        row++;
-        column = 0;
-        listOfBindings.get(row).get(column).editText.requestFocus();
-    }
-
-    private static class GenericKeyEvent implements View.OnKeyListener {
-        private final EditText currView, prevView;
-
-        public GenericKeyEvent(EditText currView, EditText prevView) {
-            this.currView = currView;
-            this.prevView = prevView;
-        }
-
-        @Override
-        public boolean onKey(View v, int keyCode, KeyEvent event) {
-            if (event.getAction() == KeyEvent.ACTION_DOWN
-                    && keyCode == KeyEvent.KEYCODE_DEL
-                    && currView.getText().toString().length() == 0) {
-                if (prevView != null) {
-                    prevView.setText(null);
-                    prevView.requestFocus();
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
-    private static class GenericTextWatcher implements TextWatcher {
-        private final EditText currView, nextView;
-
-        public GenericTextWatcher(EditText currView, EditText nextView) {
-            this.currView = currView;
-            this.nextView = nextView;
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            if (s.length() == 1) {
-                if (nextView != null) {
-                    nextView.requestFocus();
-                }
+                listOfBindings.get(i).get(j).textView.setEnabled(i == currentRow);
             }
         }
     }
--- app/src/main/java/com/chanpreet/numberninja/MainActivity.java
+++ app/src/main/java/com/chanpreet/numberninja/MainActivity.java
@@ -5,6 +5,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,11 +30,25 @@ public class MainActivity extends AppCompatActivity {
         binding.linearLayout.addView(matrixInfo.getView());
         gameInstance = new GameInstance(getApplicationContext(), matrixInfo);
 
-        gameInstance.moveToNextRow();
-        binding.equalBtn.setOnClickListener(v -> {
-            gameInstance.moveToNextRow();
-        });
+        binding.equalBtn.setOnClickListener(v -> gameInstance.CheckButtonClicked());
+        binding.backspaceBtn.setOnClickListener(v -> gameInstance.removeCharacter());
 
-        
+        binding.plusBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.minusBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.multiplyBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.divideBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.equalsBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+
+        binding.zeroBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.oneBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.twoBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.threeBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.fourBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+
+        binding.fiveBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.sixBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.sevenBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.eightBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
+        binding.nineBtn.setOnClickListener(v -> gameInstance.pushCharacter(((Button) v).getText().toString().charAt(0)));
     }
 }
